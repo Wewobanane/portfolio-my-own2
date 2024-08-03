f6ad32c501b922b42533911caee1eb4eafc71b10
wewobanane <thomaswewobanane0@gmail.com>
2024-08-02 17:23:20 -0800

    own2f commit

--- index.js
+++ index.js
@@ -22,3 +22,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     animateText();
 });
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
